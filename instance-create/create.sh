234026f3c53d1b053ddcc672e2a30747d3389a90
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-16 15:56:03 +0530

    new

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-LID=lt-00bc8565629a21a72
+LID=lt-0624a97bb5d8cca24
 LVER=1
 #COMPONENT=$1
 
@@ -24,7 +24,7 @@ Instance_Create() {
   IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z01109932OAEV19U6FDDX --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z08106881B8A3K8UKCCQD --change-batch file:///tmp/record.json
   sed -i -e "/${COMPONENT}/ d" ../inv
   echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inv
 }
